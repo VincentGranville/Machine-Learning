cf2101521c2fa2bc25db68d9ee5b8a0171aebfe6
Vincent Granville <vincentg@datashaping.com>
2022-08-20 12:26:43 -0700

    Find mixture centers, with confidence regions

--- Source Code/mixture1D.py
+++ Source Code/mixture1D.py
@@ -11,16 +11,19 @@ min_θ_A =  99999999
 min_θ_B =  99999999
 max_θ_A = -99999999
 max_θ_B = -99999999
+CR_x=[]  # confidence region for (best_θ_A, best_θ_A), 1st coordinate  
+CR_y=[]  # confidence region for (best_θ_A, best_θ_A), 2nd coordinate 
 
-for sample in range(40):
+for sample in range(5000):
 
     W_A  = np.random.normal(0.5, 0.3, size=n_A)
     W_B  = np.random.normal(1.0, 0.2, size=n_B)
     W    = np.concatenate((W_A, W_B))
     minError=99999999
     print('Sample %1d:' %(sample))
-    
+
     for iter in range(100000):
+
         θ_A = -1 + 3*np.random.rand()
         θ_B = -1 + 3*np.random.rand()
         Error = (1/n) * np.sum(abs( (W - θ_A * Ones) * (W - θ_B * Ones) )**p)
@@ -29,7 +32,7 @@ for sample in range(40):
             print('Iter = %5d  θ_A = %+.5f  θ_B = %+.5f  Error = %+.5f' %(iter,θ_A ,θ_B, Error))
             best_θ_A = min(θ_A, θ_B)
             best_θ_B = max(θ_A, θ_B)
-            
+
     if best_θ_A < min_θ_A:
         min_θ_A = best_θ_A
     if best_θ_A > max_θ_A:
@@ -38,6 +41,8 @@ for sample in range(40):
         min_θ_B = best_θ_B
     if best_θ_B > max_θ_B:
         max_θ_B = best_θ_B
+    CR_x.append(best_θ_A) 
+    CR_y.append(best_θ_B) 
     print()
  
 print('95 %% range for min(θ_A, θ_B): [%+.5f, %+.5f]' %(min_θ_A ,max_θ_A))
@@ -54,3 +59,6 @@ bins=np.linspace(min(W), max(W), num=100)
 plt.hist(W_A, color = "blue", alpha=0.3, edgecolor='black',bins=bins) 
 plt.hist(W_B, color = "red", alpha=0.3, edgecolor='black',bins=bins) 
 plt.show()
+# plotting confidence region
+plt.scatter(CR_x,CR_y,s=4) 
+plt.show() 
