379f770cff53c707e6fc3df6dfa02905fc6bc649
Vincent Granville <vincentg@datashaping.com>
2022-09-10 13:11:59 -0700

    Python code for hidden decision trees (HDT)

--- Source Code/HDT.py
+++ Source Code/HDT.py
@@ -85,9 +85,13 @@ in_file.close()
 
 nArticles = ID - 1  # -1 as the increments were done post loop
 avg_pv = sum_pv/nArticles
-articleFlag = ["BAD" for n in range(nArticles)]
+articleFlag = ["NA" for n in range(nArticles)]
 nidx = 0
 nidx_Good = 0
+nidx_Bad  = 0
+pv_threshold_good = 7.1
+pv_threshold_bad = 6.2
+mode = 'robust method'  # options are 'perfect fit' or 'robust method'
 OUT = open('hdt-out2.txt','w')
 OUT2 = open('hdt-reasons.txt','w')
 for idx in List:
@@ -97,10 +101,18 @@ for idx in List:
     Max = list_pv_max[idx]
     idlist = list_id[idx]
     nidx += 1
-    # below values are chosen based on heuristics and experimenting
-    if n > 3 and n < 8 and Min > 6.9 and Avg > 7.6 or \
-          n >= 8 and n < 16 and Min > 6.7 and Avg > 7.4 or \
-          n >= 16 and n < 200 and Min > 6.1 and Avg > 7.2:
+    if mode == 'perfect fit':
+      good_node = n > 2 and Min > pv_threshold_good
+      bad_node  = n > 2 and Max < pv_threshold_bad
+    elif mode == 'robust method': 
+        # below values are chosen based on heuristics and experimenting 
+        good_node = n > 3 and n < 8 and Min > 6.9 and Avg > 7.6 or \
+                n >= 8 and n < 16 and Min > 6.7 and Avg > 7.4 or \
+                n >= 16 and n < 200 and Min > 6.1 and Avg > 7.2
+        bad_node =  n > 3 and n < 8 and Max < 6.3 and Avg < 5.4 or \
+                n >= 8 and n < 16 and Max > 6.6 and Avg < 5.9 or \
+                n >= 16 and n < 200 and Max > 7.2 and Avg < 6.2 
+    if good_node:
         OUT.write(idx + '\t' + str(n) + '\t' + str(Avg) + '\t' + str(Min) + '\t' + str(Max) + '\t' + str(idlist) + '\n')
         nidx_Good += 1
         for ID in idlist:
@@ -108,9 +120,11 @@ for idx in List:
             pv = articlepv[ID]
             OUT2.write(title + '\t' + str(pv) + '\t' +  idx + '\t' + str(n) + '\t' + str(Avg) + '\t' + str(Min) + '\t' + str(Max) + '\n')
             articleFlag[ID] = "GOOD"
-
+    elif bad_node:
+        nidx_Bad += 1
+        for ID in idlist:
+            articleFlag[ID] = "BAD"
 # Computing results based on Threshold values
-pv_threshold = 7.1
 pv1 = 0
 pv2 = 0
 n1 = 0
@@ -124,33 +138,39 @@ for ID in range(nArticles):
     if articleFlag[ID] == "GOOD":
         n1 += 1
         pv1 += pv
-        if pv < pv_threshold:
+        if pv < pv_threshold_good:
             FalsePositive += 1
-    else:
+    elif articleFlag[ID] == "BAD":
         n2 += 1
         pv2 += pv
-        if pv > pv_threshold:
+        if pv > pv_threshold_bad:
             FalseNegative += 1
-    if pv > pv_threshold:
+    if pv > pv_threshold_good: 
         m1 += 1
-    else:
+    elif pv < pv_threshold_bad:  
         m2 += 1
 #
 # Printing results
 avg_pv1 = pv1/n1
 avg_pv2 = pv2/n2
-errorRate = (FalsePositive + FalseNegative)/nArticles
+errorRate = FalsePositive/n1
+UnclassifiedRate = 1 - (n1 + n2) / nArticles
 aggregationFactor = (nidx/nidx_Good)/(nArticles/n1)
-print ("Average pv: " + str(avg_pv))
-print ("Number of articles marked as good: ", n1, " (real number is ", m1,")", sep = "" )
-print ("Number of articles marked as bad: ", n2, " (real number is ", m2,")", sep = "")
-print ("Avg pv: articles marked as good:", avg_pv1)
-print ("Avg pv: articles marked as bad:",avg_pv2)
-print ("Number of false positive:",FalsePositive,"(bad marked as good)")
-print ("Number of false negative:", FalseNegative, "(good marked as bad)")
-print ("Number of articles:", nArticles)
-print ("Error Rate: ", errorRate)
-print ("Number of feature values: ", nidx, " (marked as good: ", nidx_Good,")", sep = "")
-print ("Aggregation factor:", aggregationFactor)
+print ("Average log pageview count (pv):","{0:.2f}".format(avg_pv))
+print ("Avg pv, articles marked as Good:","{:.2f}".format(avg_pv1))
+print ("Avg pv, articles marked as Bad :","{:.2f}".format(avg_pv2))
+print()
+print ("Number of articles marked as Good: ", n1, " (real number is ", m1,")", sep = "" )
+print ("Number of articles marked as Bad : ", n2, " (real number is ", m2,")", sep = "")
+print ("Number of false positives        :",FalsePositive,"(Bad marked as Good)")
+print ("Number of false negatives        :", FalseNegative, "(Good marked as Bad)")
+print ("Total number of articles         :", nArticles)
+print()
+print ("Proportion of False Positives: ","{0:.1%}".format(errorRate))
+print ("Proportion of Unclassified   : ","{0:.1%}".format(UnclassifiedRate))
+print()
+print ("Aggregation factor (Good node):","{:.1f}".format(aggregationFactor))
+print ("Number of feature values: ", nidx," (marked as good: ", nidx_Good,")", sep = "")
+print ()
+print("Execution time: ","{:.4f}".format(time.time() - start), "seconds")
 
-print("Execution time: " + str(time.time() - start) +"s")
